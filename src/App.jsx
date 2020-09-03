56ac0fce8b807b3c5b80dd5fb6dc956077d71110
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 03:02:32 -0300

    add accordion to show transactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Accordion, Card, Button} from 'react-bootstrap';
+
 
 function App() {
-  return (
-    <div>
-        Hello World!
-    </div>
+    const [balance, setBalance] = useState(0);
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            let bal = await fetch("http://localhost:3000");
+            bal = await bal.json();
+            setBalance(bal);
+        
+            let trans = await fetch("http://localhost:3000/transactions");
+            trans = await trans.json();
+            setTransactions(trans);
+        })();
+    }, []);
+
+    return (
+        <>
+            <header> Hello! Your balance is { balance } </header>
+            <main>
+                <Accordion>
+                    { transactions.map(transaction => {
+                        return (
+                            <Card key={transaction.id}>
+                                <Card.Header>
+                                    <Accordion.Toggle as={Button} variant="link" eventKey={transaction.id}>
+                                        {transaction.type}: USD {transaction.amount}
+                                   </Accordion.Toggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey={transaction.id}>
+                                    <Card.Body> 
+                                        <div> State: Successful </div>
+                                        <div> Id of transaction: {transaction.id} </div>
+                                        <div> Date of transaction: {transaction.effectiveDate} </div>
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        );
+                    })}
+                </Accordion>
+            </main>
+        </>
   );
 }
 
